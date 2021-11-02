a321539502407c0e97cbdbb14d5e05c5796fe292
raulito28 <raulbuenoherrera@gmail.com>
2021-11-02 19:17:36 +0100

    Update

--- Ejercicio7/main.py
+++ Ejercicio7/main.py
@@ -1,6 +1,15 @@
-lista_coches = []
+lista_telefono = list()
+
 while True:
     nombre = input("Nombre del contacto: ")
     if nombre == "fin":
         break
     telefono = input("Número de teléfono: ")
+
+    linea = {}
+    linea["Nombre"] = nombre
+    linea["Nmero de teléfono"] = telefono
+
+    lista_telefono.append(linea)
+
+print("Lista numeros d etelfono: \n", lista_telefono)
